e2f951970690eadf4ef73574bd5f1790abb022bf
liweiwei1419 <liweiwei1419@gmail.com>
2024-11-29 11:48:12 +0800

    配置了主题颜色和项目图片。

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -13,7 +13,7 @@ export default hopeTheme({
 
   iconAssets: "fontawesome-with-brands",
 
-  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
+  logo: "leetcode.jpeg",
 
   repo: "vuepress-theme-hope/vuepress-theme-hope",
 
